7cf102e1c476071acffbdd035b38d43eecf50b7e
pep carnicero <pepe.carnicero@gmail.com>
2017-11-24 17:48:20 +0100

    update url link to

--- src/components/Podcast_List/podcast_card.js
+++ src/components/Podcast_List/podcast_card.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const PodcastCard = props => (
   <div className="podcast">
-    <Link to={`/podcastInfo/${props.id.attributes['im:id']}`}>
+    <Link to={`/podcast/${props.id.attributes['im:id']}`}>
       <img className="podcast__img" src={props['im:image'][0].label} alt={props['im:name'].label} />
       <h4 className="podcast__name ">{props['im:name'].label}</h4>
       <span className="podcast__author ">Author : {props['im:artist'].label}</span>
