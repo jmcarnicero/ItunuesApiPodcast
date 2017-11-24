763fd452787e5d3cebc53d49986d2d18a7cfb55a
pep carnicero <pepe.carnicero@gmail.com>
2017-11-24 17:47:37 +0100

    added PodcastDetail logic

--- src/components/Podcast_Detail/podcast_detail.js
+++ src/components/Podcast_Detail/podcast_detail.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as actions from '../../actions';
+
+import PodcastInfo from '../podcast_info';
 
 class PodcastDetail extends Component {
   constructor(props) {
     super(props);
-    return true;
+    this.props.setPodcastSelected(this.props.params.podcastId);
   }
+
   render() {
-    return <h1>PodcastDetail</h1>;
+    const renderInfo = function () {
+      if (Object.keys(this.props.podcastSelected).length > 0) {
+        return <PodcastInfo {...this.props.podcastSelected} />;
+      }
+      return null;
+    }.bind(this);
+
+    return (
+      <div>
+        <h1>PodcastDetail</h1>
+        {renderInfo()}
+      </div>
+    );
   }
 }
 
-export default PodcastDetail;
+PodcastDetail.propTypes = {
+  setPodcastSelected: PropTypes.func.isRequired,
+  podcastSelected: PropTypes.func.isRequired,
+  params: PropTypes.shape({
+    podcastId: PropTypes.string,
+  }).isRequired,
+};
+
+function mapStateToProps(state) {
+  return { podcastSelected: state.podcastSelected };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { setPodcastSelected: id => dispatch(actions.setPodcastSelected(id)) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PodcastDetail);
