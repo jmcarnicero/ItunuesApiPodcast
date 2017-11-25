aadc1cd89a1155cd43143c1df62432164404b2db
pep carnicero <pepe.carnicero@gmail.com>
2017-11-25 14:38:17 +0100

    added podcastDetauil reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import podcastsReducer from './podcasts';
 import podcastDetailReducer from './podcast_detail';
 import podcastSelectedReducer from './podcast_selected';
+import tracksReducer from './tracks';
 
 const rootReducer = combineReducers({
   podcasts: podcastsReducer,
   podcastDetail: podcastDetailReducer,
   podcastSelected: podcastSelectedReducer,
+  tracks: tracksReducer,
 });
 
 export default rootReducer;
--- src/reducers/podcast_detail.js
+++ src/reducers/podcast_detail.js
@@ -1,10 +1,9 @@
 import { FETCH_PODCAST_DETAIL } from '../actions/types';
 
-function podcastDetailReducer(state = [], action) {
+function podcastDetailReducer(state = {}, action) {
   switch (action.type) {
     case FETCH_PODCAST_DETAIL: {
-      // return [...state, action.payload.data.results];
-      return Object.assign({}, state, action.payload.data.results);
+      return Object.assign({}, state, action.payload.data.results[0]);
     }
 
     default:
