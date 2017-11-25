14e7ff24abc1323e0d48eeebb9ddedecbc827cb9
pep carnicero <pepe.carnicero@gmail.com>
2017-11-25 14:27:44 +0100

    refactor to pure function

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,18 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import PropTypes from 'prop-types';
-import * as actions from '../actions';
 import '../styles/App.css';
 
-class App extends Component {
-  static propTypes = {
-    fetchPodcasts: PropTypes.func.isRequired,
-  };
+const App = props => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Podcasters</h1>
+    </header>
+    {props.children}
+  </div>
+);
 
-  componentWillMount() {
-    this.props.fetchPodcasts();
-  }
+App.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Podcasters</h1>
-        </header>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  podcasts: state.podcasts,
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchPodcasts: () => dispatch(actions.fetchPodcasts()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
