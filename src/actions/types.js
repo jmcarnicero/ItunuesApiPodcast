0db72cbca42079822f4aab339319134e4fc11015
pep carnicero <pepe.carnicero@gmail.com>
2017-11-25 14:26:59 +0100

    added type SET_TRACKS

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 const FETCH_PODCAST = 'fetch_podcast';
 const FETCH_PODCAST_DETAIL = 'fetch_podcast_detail';
 const SET_PODCAST_SELECTED = 'set_podcast_selected';
-export { FETCH_PODCAST, FETCH_PODCAST_DETAIL, SET_PODCAST_SELECTED };
+const SET_TRACKS = 'set_traks';
+export { FETCH_PODCAST, FETCH_PODCAST_DETAIL, SET_PODCAST_SELECTED, SET_TRACKS };
