2446dd5eb5d28df743ab435ceb6f93804dbc295d
pep carnicero <pepe.carnicero@gmail.com>
2017-11-25 14:25:47 +0100

    added fetchFeed & ParseXml

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { FETCH_PODCAST, FETCH_PODCAST_DETAIL, SET_PODCAST_SELECTED } from './types';
+import { parseString } from 'xml2js';
+import { FETCH_PODCAST, FETCH_PODCAST_DETAIL, SET_PODCAST_SELECTED, SET_TRACKS } from './types';
 
 export function fetchPodcasts() {
   return (dispatch) => {
@@ -29,11 +30,43 @@ export function setPodcastSelected(id) {
 export function fetchPodcastDetail(id) {
   return (dispatch) => {
     const url = `https://itunes.apple.com/lookup?id=${id}`;
-    axios.get(url).then((response) => {
-      dispatch({
-        type: FETCH_PODCAST_DETAIL,
-        payload: response,
+    axios
+      .get(url)
+      .then((response) => {
+        dispatch({
+          type: FETCH_PODCAST_DETAIL,
+          payload: response,
+        });
+      })
+      .catch((error) => {
+        console.log('error', error);
       });
-    });
+  };
+}
+
+const parseXml = (xml) => {
+  let result = [];
+  parseString(xml, (err, data) => {
+    result = data.rss.channel[0].item;
+  });
+
+  return result;
+};
+
+export function fetchFeeds(url) {
+  return (dispatch) => {
+    const completeUrl = `https://cors-anywhere.herokuapp.com/${url}`;
+    axios
+      .get(completeUrl)
+      .then(response => response.data)
+      .then((data) => {
+        dispatch({
+          type: SET_TRACKS,
+          payload: parseXml(data),
+        });
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
   };
 }
