2b4209d32831133f799bf10a0b37a3cd47d84c0b
pep carnicero <pepe.carnicero@gmail.com>
2017-11-25 14:36:05 +0100

    minior refactor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
+import App from './components/App';
 import PodcastList from './components/Podcast_List/podcats_list';
 import PodcastDetail from './components/Podcast_Detail/podcast_detail';
 import PodcastChapter from './components/Podcast_Chapter/podcast_chapter';
@@ -20,12 +21,11 @@ const store = createStoreWithMiddleware(reducers);
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
-      {
-        // <Route path="/" component={App} />
-      }
-      <Route path="/" component={PodcastList} />
-      <Route path="/podcast/:podcastId" component={PodcastDetail} />
-      <Route path="/podcast/:podcastId/episode/:episodeId" component={PodcastChapter} />
+      <Route path="/" component={App}>
+        <IndexRoute component={PodcastList} />
+        <Route path="/podcast/:podcastId" component={PodcastDetail} />
+        <Route path="/podcast/:podcastId/episode/:episodeId" component={PodcastChapter} />
+      </Route>
     </Router>
   </Provider>,
   document.getElementById('root'),
